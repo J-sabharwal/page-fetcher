933a2a285f82f50bac7ab62a7f05eca331a99f47
Jaspinder Sabharwal <jas@jaspinder.net>
2020-03-12 16:19:23 +0000

    added fetcher to take in a webpage and a filed path to request body of html for that webpage

---
+++ fetcher.js
@@ -0,0 +1,17 @@
+const net = require('net');
+const request = require('request');
+const fs = require('fs')
+
+let arg = process.argv.slice(2);
+
+request(arg[0], (error, response, body) => {
+  //console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+
+  fs.writeFile(arg[1], body, (err) => {
+    console.log("Completed")
+  });
+});
+
+
